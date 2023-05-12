f34d78ce9a814baf92c86c7e0f1abee2923422a9
zakaria <zakaria.mzaouali@gmail.com>
2023-05-12 07:26:34 +0200

    dynamics

--- ham.jl
+++ ham.jl
@@ -7,14 +7,25 @@ pygui(true)
 
 
 
-function Ham(α::Float64,j::Int)
+function Ham(h::Float64,j::Int)
     N=2*j;
     b=SpinBasis(N);
-    hamiltonian=-(1/N) * (sigmax(b)^2) + α*(sigmaz(b)+(N/2)*identityoperator(b));
-    e=eigenenergies(dense(hamiltonian))/N
+    #vi=normalize(spinup(b) + spindown(b))
+    #vi=normalize(Ket(b,fill(1,2*N+1)) + Ket(b,fill(-1,2*N+1)))
+    hamiltonian=-(1/N) * (sigmax(b)^2) + h*(sigmaz(b)+(N/2)*identityoperator(b));
+    return hamiltonian
 end
 
-j=5
-α=range(0,2,100)
-plot(α, Ham.(α,j))
+function echo(hi,hf,t)
+    ei,vi=eigenstates(Ham(hi,j),info=false)
+    echo=abs(expect(exp(dense(-1im*t*Ham(hf,j))),vi[1]))^2
+    return echo
+end
+
+
+j=10
+hi=0.5
+hf=0.75
+t=range(0,40,100)
+plot(t, echo.(hi,hf,t))
 PyPlot.show()
