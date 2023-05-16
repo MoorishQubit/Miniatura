a81ae5f4d2a267a89256ff881f87ed6362006f00
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-05-16 16:43:52 +0200

    threading

--- ham.jl
+++ ham.jl
@@ -1,32 +1,35 @@
-ENV["MPLBACKEND"]="tkagg";
-using QuantumOptics, PyPlot, Arpack
-
-rc("font", family="serif")
+ENV["MPLBACKEND"]="tkagg"
+using QuantumOptics, PyPlot, Arpack, ProgressMeter, LinearAlgebra, NPZ
 
 pygui(true)
+rc("font", family="serif")
 
 
+
+BLAS.set_num_threads(20)
 
 function Ham(h::Float64,j::Int)
     N=2*j;
     b=SpinBasis(N);
-    #vi=normalize(spinup(b) + spindown(b))
-    #vi=normalize(Ket(b,fill(1,2*N+1)) + Ket(b,fill(-1,2*N+1)))
     hamiltonian=-(1/N) * (sigmax(b)^2) + h*(sigmaz(b)+(N/2)*identityoperator(b));
     return hamiltonian
 end
 
 function echo(hi,hf,j,t)
-    e,v=eigs(Ham(hi,j).data,nev=1,which=:SM)
-    vi=Ket(SpinBasis(2*j),v)
-    echo=abs(expect(exp(dense(-1im*t*Ham(hf,j))),vi))^2
-    return echo
+    data=[]
+    p = Progress(length(t), "Calculating LE for N=$(2*j)")
+    ei,vi=eigenstates(Ham(hi,j),nev=1,which=:SM, info=false)
+    Threads.@threads for i ∈ t
+        l=abs(expect(exp(-1im*dense(Ham(hf,j))*i),vi)[1])^2
+        push!(data,l)
+        next!(p)
+    end
+    finish!(p)
+    return data
 end
 
-
-j=100
-hi=0.5
-hf=0.75
-t=range(0,40,100)
-plot(t, echo.(hi,hf,j,t))
+j=100; hi=0.5; hf=0.75;
+t=range(0,5,50)
+# echo(hi,hf,j,t)
+PyPlot.plot(t,echo(hi,hf,j,t))
 PyPlot.show()
